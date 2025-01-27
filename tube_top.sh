a21eb33979bc1af037892933104c1e533a692483
am2b <20837051+am2b@users.noreply.github.com>
2025-01-28 00:23:39 +0800

    finish basic functions

--- tube_top.sh
+++ tube_top.sh
@@ -6,23 +6,12 @@ script=$(basename "$0")
 root_dir="${HOME}"/.tube-top
 books_dir="${root_dir}"/books
 cache_dir="${root_dir}"/cache
-#progress_dir="${root_dir}"/progress
-
-declare -A tube_top_columns
-tube_top_columns["book_name"]=1
-tube_top_columns["reading"]=2
-tube_top_columns["total_lines"]=3
-tube_top_columns["current_line"]=4
 
 #records of books
 tube_top="${root_dir}"/tube_top
 
-#by default,10 lines are printed
-#show_lines_number=10
-show_lines_number=2
-
-#cache_lines_number=1000
 cache_lines_number=10
+show_lines_number=3
 
 required_tools() {
     local tools=("sed" "awk")
@@ -41,13 +30,16 @@ required_tools() {
 usage() {
     echo "usage:"
     echo "${script} -h:show usage"
-    #initialize a book and start reading from the beginning
-    echo "${script} -i book_file:init a book to read from the beginning"
-    #pick up a book you've read before and continue reading
-    echo "${script} -s book_name:start to read a book"
+    #add a book
+    echo "${script} -i book_file:add a book"
+    #read a book
+    echo "${script} -s book_name:read a book"
+
     echo "${script} -j line_number:jump to line_number"
+    echo "${script} -j +lines:jump backward lines"
+    echo "${script} -j -lines:jump forward lines"
+
     echo "${script} -d book_name:delete a book"
-    echo "${script}:continue reading where you left off"
     echo "${script} -l:list the books you have read"
     echo "${script} -c:clear all settings and caches"
     exit 0
@@ -55,15 +47,16 @@ usage() {
 
 parse_options() {
     while getopts ":hi:s:j:d:lc" opt; do
+        #while getopts ":hi:j:d:lc" opt; do
         case "${opt}" in
         h)
             usage
             ;;
         i)
-            init_book "$OPTARG"
+            add_book "$OPTARG"
             ;;
         s)
-            start_to_read "$OPTARG"
+            print "$OPTARG"
             ;;
         j)
             jump_to_line "$OPTARG"
@@ -92,6 +85,25 @@ _init() {
     if [[ ! -d $cache_dir ]]; then mkdir -p "${cache_dir}"; fi
 
     touch "${tube_top}"
+    #echo "cache_lines_number=10" >>config
+    #echo "show_lines_number=3" >>config
+    #echo "color=green" >>config
+}
+
+_save_book_file() {
+    local book_file
+    book_file="${1}"
+    local book_name
+    book_name=$(basename "$book_file")
+
+    #check if the library already has this book
+    if _get_book_file "${book_name}" || _query_book_in_tube_top "${book_name}"; then
+        echo "error:the library already contains a book with the same name:${book_name}"
+        exit 1
+    fi
+
+    #save this book to the library
+    cp "${book_file}" "${books_dir}"
 }
 
 _get_book_file() {
@@ -139,7 +151,9 @@ _query_book_in_tube_top() {
 
 _query_the_reading_book_in_tube_top() {
     local result
-    result=$(sed -n '/^\([^,]*,\)\{3\}true,/p' "${tube_top}")
+    #表示reading的true或false位于第2列
+    #\([^,]*,\):匹配一个组,即非逗号的字符([^,])的0或多个重复,紧跟一个逗号(,)
+    result=$(sed -n '/^\([^,]*,\)true,/p' "${tube_top}")
     if [[ -n $result ]]; then
         echo "${result}"
         return 0
@@ -148,13 +162,23 @@ _query_the_reading_book_in_tube_top() {
     fi
 }
 
+#列出已经读完的书
+_list_the_finished_book_in_tube_top() {
+    local result
+    #表示finish的true或false位于第7列
+    sed -n '/^\([^,]*,\)\{6\}true,/p' "${tube_top}"
+}
+
 _add_book_in_tube_top() {
     local book_name="${1}"
-    local total_lines="${2}"
-    local current_line="${3}"
-    local reading="${4}"
+    local reading="${2}"
+    local total_lines="${3}"
+    local current_line="${4}"
+    local total_cache_lines="${5}"
+    local current_cache_line="${6}"
+    local finish="${7}"
     if ! book=$(_query_book_in_tube_top "${book_name}"); then
-        echo "${book_name}","${total_lines}","${current_line}","${reading}" >> "${tube_top}"
+        echo "${book_name}","${reading}","${total_lines}","${current_line}","${total_cache_lines}","${current_cache_line}","${finish}" >>"${tube_top}"
     else
         echo "error:the book:${book_name} was found,when adding in ${tube_top}"
         exit 1
@@ -163,13 +187,18 @@ _add_book_in_tube_top() {
 
 _delete_book_from_tube_top() {
     local book_name="${1}"
-    sed -i '' "/^$book_name,/d" "${tube_top}"
+    if ! book=$(_query_book_in_tube_top "${book_name}"); then
+        echo "error:the book:${book_name} was not found,when deleting in ${tube_top}"
+    else
+        sed -i "/^$book_name,/d" "${tube_top}"
+    fi
 }
 
 _update_book_in_tube_top() {
     local book_name="${1}"
     local modify_column="${2}"
     local new_value="${3}"
+
     if book=$(_query_book_in_tube_top "${book_name}"); then
         IFS=',' read -r -a parts <<<"${book}"
 
@@ -202,44 +231,51 @@ _update_book_in_tube_top() {
 
 _cache() {
     local book_name="${1}"
+    local book_file
+    local book_cache_file="${cache_dir}"/"${book_name}"
 
     if book=$(_query_book_in_tube_top "${book_name}"); then
         IFS=',' read -r -a parts <<<"${book}"
-        local total_lines="${parts[1]}"
-        local current_line="${parts[2]}"
-        local cache_lines_count="${cache_lines_number}"
+        local total_lines="${parts[2]}"
+        local current_line="${parts[3]}"
 
+        local cache_lines_count
         #check if current_line+cache_lines_number exceeds the total lines
-        if ((current_line + cache_lines_number > total_lines)); then
-            cache_lines_count=$((total_lines - current_line))
+        if ((current_line + cache_lines_number - 1 > total_lines)); then
+            cache_lines_count=$((total_lines - current_line + 1))
+        else
+            cache_lines_count="${cache_lines_number}"
         fi
 
-        local book_file
         if ! book_file=$(_get_book_file "${book_name}"); then
             echo "error:the book file:${book_file} was not found in ${books_dir} when caching"
             exit 1
         fi
 
-        local book_cache_file="${cache_dir}"/"${book_name}"
         awk "NR>=${current_line} && NR<${current_line}+${cache_lines_count}" "${book_file}" >"${book_cache_file}"
+
+        #update total cache lines and current cache line
+        local total_cache_lines
+        total_cache_lines=$(wc -l <"${book_cache_file}" | xargs)
+        _update_book_in_tube_top "${book_name}" 5 "${total_cache_lines}"
+        _update_book_in_tube_top "${book_name}" 6 1
+
+        #update current line in the entire book
+        _update_book_in_tube_top "${book_name}" 4 $((current_line + cache_lines_count))
     else
         echo "error:the book:${book_name} was not found in ${tube_top} when caching"
         exit 1
     fi
 }
 
-init_book() {
-    if (( $# != 1)); then
+add_book() {
+    if (($# != 1)); then
         echo "init a book to read from the beginning:"
         echo "$script -i book_file"
         exit 1
     fi
 
     local book_file="${1}"
-
-    if [[ ! -f "${tube_top}" ]]; then
-        _init
-    fi
 
     if [[ ! -f $book_file ]]; then
         echo "$book_file is not a normal file"
@@ -256,54 +292,128 @@ init_book() {
         exit 1
     fi
 
-    book_name=$(basename "$book_file")
-    #check if the library already has this book
-    if _get_book_file "${book_name}"; then
-        echo "error:the library already contains a book with the same name:${book_name}"
-        exit 1
+    if [[ ! -f "${tube_top}" ]]; then
+        _init
     fi
 
-    #save this book to the library
-    cp "${book_file}" "${books_dir}"
+    _save_book_file "${book_file}"
 
     #register this book
-    total_lines=$(wc -l <"${1}" | xargs)
-    current_line=1
-    _add_book_in_tube_top "${book_name}" "${total_lines}" "${current_line}" false
+    local book_name
+    book_name=$(basename "$book_file")
+    local reading=false
+    local total_lines
+    total_lines=$(wc -l <"${book_file}" | xargs)
+    local current_line=1
+    local total_cache_lines=0
+    local current_cache_line=0
+    local finish=false
+    _add_book_in_tube_top "${book_name}" "${reading}" "${total_lines}" "${current_line}" "${total_cache_lines}" "${current_cache_line}" "${finish}"
+
+    return 0
 }
 
-start_to_read() {
-    if (( $# != 1)); then
+print() {
+    if (($# != 1)); then
         echo "start to read a book:"
         echo "$script -s book_name"
         exit 1
     fi
 
-    book_name="${1}"
+    local book_name="${1}"
 
-    if ! _query_book_in_tube_top "${book_name}" > /dev/null; then
-        echo "the book:${book_name} you chose to read is not in the library"
-        echo "you should init a book first:"
-        echo "$script -i book_file"
+    if ! book=$(_query_book_in_tube_top "${book_name}"); then
+        echo "error:the book:${book_name} was not found,when deleting in ${tube_top}"
         exit 1
     fi
 
-    #change the reading status of other books to false
-    sed -i '' -E 's/^(([^,]*,){3})[^,]*/\1false/' "${tube_top}"
-    #set the reading status of this book to true
-    _update_book_in_tube_top "${book_name}" 4 true
+    local last_reading_book_name
+    last_reading_book_name=$(_query_the_reading_book_in_tube_top)
+    if [[ "${book_name}" != "${last_reading_book_name}" ]]; then
+        #change the reading status of other books to false
+        #-E:启用扩展正则表达式(ERE),使正则表达式语法更简洁(无需转义括号()等)
+        #([^,]*,):
+        #([^,]*,):匹配非逗号的任意字符([^,],非逗号),后跟一个逗号(,)
+        #[^,]*:匹配接下来的字段(直到下一个逗号或行尾)
+        #\1false:
+        #\1:表示正则表达式中第一个捕获组([^,]*,),即匹配的第一个字段加逗号
+        #false:将第二字段替换为false
+        sed -i -E 's/^(([^,]*,))[^,]*/\1false/' "${tube_top}"
 
-    #make a cache of this book
-    _cache "${book_name}"
+        #set the reading status of this book to true
+        _update_book_in_tube_top "${book_name}" 2 true
+    fi
+
+    if [[ -z $(_get_book_cache_file "${book_name}") ]]; then
+        #make a cache of this book
+        _cache "${book_name}"
+    fi
+    local book_cache_file
+    book_cache_file=$(_get_book_cache_file "${book_name}")
+
+    book=$(_query_book_in_tube_top "${book_name}")
+    IFS=',' read -r -a parts <<<"${book}"
+    local total_lines="${parts[2]}"
+    local current_line="${parts[3]}"
+    local total_cache_lines="${parts[4]}"
+    local current_cache_line="${parts[5]}"
+    local finish="${parts[6]}"
+
+    if [[ "${finish}" == true ]]; then
+        echo "You have finished the book:${book_name}"
+        exit 0
+    fi
+
+    #是否需要回退
+    if ((current_cache_line + show_lines_number - 1 > total_cache_lines)); then
+        #是否能够回退
+        if [[ "${current_line}" -lt "${total_lines}" ]]; then
+            #回退current line in the entire book
+            current_line_back_count=$((total_cache_lines - current_cache_line + 1))
+            current_line=$((current_line - current_line_back_count))
+
+            _update_book_in_tube_top "${book_name}" 4 "${current_line}"
+            _cache "${book_name}"
+
+            #更新变量
+            book=$(_query_book_in_tube_top "${book_name}")
+            IFS=',' read -r -a parts <<<"${book}"
+            local total_lines="${parts[2]}"
+            local current_line="${parts[3]}"
+            local total_cache_lines="${parts[4]}"
+            local current_cache_line="${parts[5]}"
+        fi
+    fi
+
+    local left_cache_lines
+    left_cache_lines=$((total_cache_lines - current_cache_line + 1))
+    local show_lines_real_number
+    if [[ "${left_cache_lines}" -lt "${show_lines_number}" ]]; then
+        show_lines_real_number="${left_cache_lines}"
+    else
+        show_lines_real_number="${show_lines_number}"
+    fi
+
+    if [[ "${show_lines_real_number}" -ne 0 ]]; then
+        tail -n +"${current_cache_line}" "${book_cache_file}" | head -n "${show_lines_real_number}"
+        #update current cache line
+        current_cache_line=$((current_cache_line + show_lines_real_number))
+        _update_book_in_tube_top "${book_name}" 6 "${current_cache_line}"
+    fi
+
+    if [[ "${current_line}" -gt "${total_lines}" && "${current_cache_line}" -gt "${total_cache_lines}" ]]; then
+        #update the finish flag
+        _update_book_in_tube_top "${book_name}" 7 true
+        echo "You have finished the book:${book_name}"
+        exit 0
+    fi
+
+    return 0
 }
 
-#print() {
-#}
-
-#clear() {
-#}
-
 main() {
+    required_tools
+
     parse_options "${@}"
 }
 
