b86737d2fd22ec6796428a5867194d3f50fdceda
am2b <20837051+am2b@users.noreply.github.com>
2025-02-26 14:41:26 +0800

    format list.sh

--- list.sh
+++ list.sh
@@ -6,16 +6,16 @@ source "${SELF_ABS_DIR}"/global_variables.sh
 #列出已经读完的书
 list_all_books() {
     awk -F, '{
-    output = $1;
-    if ($3 == "true" && $8 == "true") {
-        output = output " <- reading - finish";
-    } else if ($3 == "true") {
-        output = output " <- reading";
-    } else if ($8 == "true") {
-        output = output " <- finish";
-    }
-    print output;
-}' "${TUBE_TOP}"
+        output = $1;
+        if ($3 == "true" && $8 == "true") {
+            output = output " <- reading - finish";
+        } else if ($3 == "true") {
+            output = output " <- reading";
+        } else if ($8 == "true") {
+            output = output " <- finish";
+        }
+        print output;
+    }' "${TUBE_TOP}"
 
     #仅查询,无需写入记录
     exit 0
