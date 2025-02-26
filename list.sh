a24dfdcc5dd1c17f585d8c8ad836e228df19de6c
am2b <20837051+am2b@users.noreply.github.com>
2025-02-27 05:47:02 +0800

    output alias when listing

--- list.sh
+++ list.sh
@@ -6,7 +6,7 @@ source "${SELF_ABS_DIR}"/global_variables.sh
 #列出已经读完的书
 list_all_books() {
     awk -F, '{
-        output = $1;
+        output = "["$2"]" " "$1;
         if ($3 == "true" && $8 == "true") {
             output = output " <- reading - finish";
         } else if ($3 == "true") {
