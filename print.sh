ab2ae973b44ed2a43294f69c3c563d6450d79e6e
am2b <20837051+am2b@users.noreply.github.com>
2025-03-04 02:14:17 +0800

    update print again

--- print.sh
+++ print.sh
@@ -158,10 +158,10 @@ print() {
 
 print_last_again() {
     local adjust_lines_number
-    adjust_lines_number=$((show_lines_number))
+    adjust_lines_number="${show_lines_number}"
 
     #注意:不要添加双引号
-    jump -${adjust_lines_number} && _write_record_to_tupe_top
+    jump -${adjust_lines_number}
 
     print
 }
