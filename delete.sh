783bd317d9fc6c0298776eb9a77fce54141c35fe
am2b <20837051+am2b@users.noreply.github.com>
2025-03-19 16:53:35 +0800

    modify delete.sh:update the reading book after delete

--- delete.sh
+++ delete.sh
@@ -15,6 +15,17 @@ delete_book() {
 
     _delete_book_from_tube_top
 
-    #避免再次将记录写入
+    #删除完成后,检查是否存在reading的书
+    local reading_book_name
+    reading_book_name=$(_get_the_reading_book_name)
+    if [[ -z "${reading_book_name}" ]]; then
+        #再检查是否有previous,如果有的话,将其设置为reading
+        local previous_book_name
+        previous_book_name=$(_get_the_previous_reading_book_name)
+        if [[ -n "${previous_book_name}" ]]; then
+            pin "${previous_book_name}"
+        fi
+    fi
+
     exit 0
 }
