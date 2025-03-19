a225178fb18b8accc6553030e33a33034c37a140
am2b <20837051+am2b@users.noreply.github.com>
2025-03-19 16:16:44 +0800

    fix:check book name of previous reading

--- quickly_switch.sh
+++ quickly_switch.sh
@@ -9,9 +9,15 @@ quickly_switch() {
 
     local previous_book_name
     previous_book_name=$(_get_the_previous_reading_book_name)
-    pin "${previous_book_name}"
+    if [[ -n "${previous_book_name}" ]]; then
+        pin "${previous_book_name}"
+        print_last_again
 
-    print_last_again
-
-    exit 0
+        exit 0
+    else
+        echo "error:there is no previous book"
+        echo "usage: you can execute the following command to set the book you are reading:"
+        echo "tube_top.sh -p book_name"
+        exit 1
+    fi
 }
